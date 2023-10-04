8f8605bb22baeb23ec1af1cd187fdb8f48bdfdd4
git <eae966@gmail.com>
2023-10-04 17:16:33 +0330

    Public area theme hostler has been added.

---
+++ src/views/public-area/public-area.tsx
@@ -0,0 +1,21 @@
+import './bootstrap.css'
+import './fontawesome.css'
+import './themify-icons.css'
+import './magnific.css'
+import './animate.css'
+import './jq-slider.css'
+import './jq-megamenu.css'
+import './owlcarousel.css'
+import './main-rtl.css'
+import './custom.css'
+
+
+const publicArea = () => {
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default publicArea;
