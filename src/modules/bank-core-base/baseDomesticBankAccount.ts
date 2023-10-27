ce92792c31c0f830b12e089c8996da5b8f6ef48d
git <eae966@gmail.com>
2023-10-27 22:15:00 +0330

    BaseDomesticBankAccount eslint has been fixed.

--- src/modules/bank-core-base/baseDomesticBankAccount.ts
+++ src/modules/bank-core-base/baseDomesticBankAccount.ts
@@ -1,23 +1,23 @@
 export abstract class BaseDomesticBankAccount {
-    public readonly bankId: number
+    public readonly bankId: number;
 
-    public readonly englishName: string
+    public readonly englishName: string;
 
-    public readonly persianName: string
+    public readonly persianName: string;
 
-    public readonly accountOwner: string
+    public readonly accountOwner: string;
 
-    public readonly accountNumber: string
+    public readonly accountNumber: string;
 
-    public readonly sheba: string
+    public readonly sheba: string;
 
-    public readonly cardNumber: string
+    public readonly cardNumber: string;
 
-    public readonly _imageUrl?: string
+    public readonly _imageUrl?: string;
 
-    public readonly isActive: boolean
+    public readonly isActive: boolean;
 
-    protected constructor(
+    protected constructor (
         bankId: number,
         englishName: string,
         persianName: string,
@@ -28,17 +28,15 @@ export abstract class BaseDomesticBankAccount {
         isActive: boolean,
         imageUrl?: string
     ) {
-        this.bankId = bankId
-        this.englishName = englishName
-        this.persianName = persianName
-        this.accountOwner = accountOwner
-        this.accountNumber = accountNumber
-        this.sheba = sheba
-        this.cardNumber = cardNumber
-        this._imageUrl = imageUrl
-        this.isActive = isActive
-    }
-
-    public abstract get imageUrl(): string
-
-}
+        this.bankId = bankId;
+        this.englishName = englishName;
+        this.persianName = persianName;
+        this.accountOwner = accountOwner;
+        this.accountNumber = accountNumber;
+        this.sheba = sheba;
+        this.cardNumber = cardNumber;
+        this._imageUrl = imageUrl;
+        this.isActive = isActive;
+    };
+    public abstract get imageUrl (): string;
+};
