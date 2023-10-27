95c1f91d816a6fb0ffe800f69bd962be734f5c3c
git <eae966@gmail.com>
2023-10-27 22:09:00 +0330

    SiteConfigs eslint has been fixed.

--- src/constants/SiteConfigs.ts
+++ src/constants/SiteConfigs.ts
@@ -3,18 +3,17 @@ export const siteConfig = {
     siteDomain: 'mrdomain.ir',
     companyAddress: 'ایران - تهران - خیابان آیت الله کاشانی نبش خیابان دانشور برج دیپلمات',
     companyPhone: '021 9101 4745',
-    siteEmail: 'info@mrdomain.ir',
-}
+    siteEmail: 'info@mrdomain.ir'
+};
 
 export const priceConfig = {
     minimumDomainPriceInUsd: 1000,
-    usdInTomanMinimum: 55000,
-}
+    usdInTomanMinimum: 55000
+};
 
 export const userDetails = {
     mobile1: '0920 383 5166',
     mobile2: '0935 387 5166',
     phone1: '021 9101 4745',
-    phone2: '021 9101 5166',
-
-}
+    phone2: '021 9101 5166'
+};
